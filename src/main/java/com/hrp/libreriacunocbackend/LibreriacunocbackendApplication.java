eab00c0fb54afacf1ba64378762243cd8e859132
chejohrpp <sergioroherp@gmail.com>
2024-04-28 23:33:02 -0600

    backend initializate

---
+++ src/main/java/com/hrp/libreriacunocbackend/LibreriacunocbackendApplication.java
@@ -0,0 +1,13 @@
+package com.hrp.libreriacunocbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriacunocbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriacunocbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrp/libreriacunocbackend/LibreriacunocbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrp.libreriacunocbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriacunocbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
